144d82b02e00237dcd3078dcc29f9f935874374c
Anthony Paragas <anthony.paragas@turner.com>
2014-12-15 15:49:11 -0500

    Push Update

---
+++ knife.rb
@@ -0,0 +1,10 @@
+log_level                :info
+log_location             STDOUT
+node_name                'sohk220'
+client_key               '/Users/aparagas/Projects/sohk220.pem'
+validation_client_name   'chef-validator'
+validation_key           '/Users/aparagas/.ssh/sohk220.pem'
+#chef_server_url          'https://cnnny-bestanthony:443'
+chef_server_url          'https://github.com/sohk220/Projects'
+syntax_check_cache_path  '/Users/aparagas/Projects/syntax_check_cache'
+cookbook_path [ '/Users/aparagas/Projects/cookbooks' ]
